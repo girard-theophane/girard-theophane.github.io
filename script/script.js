b54837e6deae3cd46709b3ff722a0f133ba2c5c8
girard-theophane <girard.theophane@gmail.com>
2015-03-09 19:18:47 +0100

    f

--- script/script.js
+++ script/script.js
@@ -20,9 +20,10 @@ $(function(){
 
 			//fait apparaitre la categorie souhaitée
 			$($idClique).delay(750).slideToggle();
-			$('#contact').('a').first().delay(2000).show('slow', function showNextOne() {
+			$('#contact').('a').hide();
+			/*$('#contact').('a').first().delay(2000).show('slow', function showNextOne() {
 			$(this).next('#contact a').show('slow', showNextOne);
-			});
+			});*/
 			$($idClique).addClass('active');
 		});	
 
